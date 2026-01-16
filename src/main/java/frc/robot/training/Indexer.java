3990111eb5f9df62f78727c624db6389d85411b6
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2026-01-15 22:27:08 -0500

    Refactor Indexer class structure: move methods outside constructor and clean up comments

--- src/main/java/frc/robot/training/Indexer.java
+++ src/main/java/frc/robot/training/Indexer.java
@@ -1,38 +1,13 @@
-package frc.robot.training;
-
 /*
  * AUTHOR: @Joel-Trumpet-67
-Good things you're doing:
-
-- You're attempting to follow the structure of the HyperDrive example
-- You have the right imports and are extending SubsystemBase correctly
-- You're thinking about multiple motors for your indexer (which is realistic)
-Issues to investigate and fix:
-Syntax Errors - Your code won't compile. Look carefully at:
-
-Line 17
-Line 28-29: name mismatches (see also lines 19-23)
-Line 31: Method definition location - check your curly braces!
-Organizational Structure - Compare your file to HyperDrive:
-
-Methods should be OUTSIDE the constructor, not inside it
-The constructor should close around line 30, then your methods start
-Copy-Paste Awareness - You have leftover references to "HyperDrive" concepts:
-
-Lines 59, 71: "isMotivatorFunctional" and "isInHyperspace" - should these be renamed for an Indexer?
-Line 84: Calling emergencyStop() but you defined emergancyStop() (typo on line 52)
-Missing Constants - You reference variables that don't exist:
-
-CHARGE_SPEED, IDLE_SPEED, ENGAGE_SPEED, READY_VELOCITY_THRESHOLD
-Look at HyperDrive lines 25-36 for how to declare these
-Think about your subsystem - You declared 4 motors but your methods only use one called "IndexerMotor" that doesn't exist. Do you need all 4 motors? How should they work together?
-
-Tip: Start by getting the basic structure to compile first, then customize for your indexer's actual behavior.
  */
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+package frc.robot.training;
+
 import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase {
 
@@ -50,73 +25,90 @@ public class Indexer extends SubsystemBase {
 
   private boolean isEngaged = false;
 
+  /*
+   * The constructor in a Java subsystem is where you set up and configure everything your subsystem needs before it starts operating.
+   * It runs exactly once when the robot code starts up (during robotInit()).
+   */
   public Indexer() { 
     IndexerMotor1 = new TalonFX(Indexer_MOTOR_ID_1);
     IndexerMotor2 = new TalonFX(Indexer_MOTOR_ID_2);
     IndexerMotor3 = new TalonFX(Indexer_MOTOR_ID_3);
     IndexerMotor4 = new TalonFX(Indexer_MOTOR_ID_4);
     DistanceSensor = new DigitalInput(Distance_SENSOR_PORT);
+  } // Need to finish your constructor before getting into methods
+  // I added a curly bracket
 
     public void go() { 
-
+/* FIXME
       if (isMotivatorFunctional()) {
-            IndexerMotor1.set(1);
+            IndexerMotor1.set(1); // Why set speeds to 1,2,3,4?
             IndexerMotor2.set(2);
             IndexerMotor3.set(3);
             IndexerMotor4.set(4);
         } else {
            
-            IndexerMotor.set(1);
+            // FIXME name doesn't match any of your motors above. e.g. IndexerMotor1
+            // IndexerMotor.set(1);
             System.out.println("WARNING: Indexer motivator malfunction!");
         }
+*/
     }
 
+    // FIXME
     public void engage() {}
-
+/* 
       if (isReady() && isMotivatorFunctional()) {
             IndexerMotor1.set(ENGAGE_SPEED);
             isEngaged = true;
         } else {
             System.out.println("WARNING: Indexer not ready for engagement!");
         }
-    
-    
+  
+*/
+
+    // FIXME
     public void emergancyStop() {
-       IndexerMotor1.set(IDLE_SPEED);
+
+        // IndexerMotor1.set(IDLE_SPEED);
         isEngaged = false;
         System.out.println("Indexer EMERGENCY STOP ACTIVATED");
     }
 
 
-    public boolean isMottivatorFunctional() {
-      return !motivatorSensor.get();
+    // FIXME
+/*
+    public boolean isMotivatorFunctional() {
+       return !motivatorSensor.get();
 
     }
+*/
 
-     public boolean isReady() {
+/* FIXME
+    public boolean isReady() {
         double currentVelocity = IndexerMotor.getVelocity().getValueAsDouble();
         boolean hasVelocity = currentVelocity >= READY_VELOCITY_THRESHOLD;
         
         return hasVelocity && isMotivatorFunctional();
     }
+*/
   
     public boolean isInHyperspace() {
         return isEngaged;
     }
     
+/* FIXME
     public double getVelocity() {
         return IndexerMotor.getVelocity().getValueAsDouble();
     }
+*/
 
     @Override
       public void periodic() {
 
-        if (isEngaged && !isMotivatorFunctional()) {
-            System.out.println("CRITICAL: Motivator failure during hyperspace!");
-            emergencyStop();
+        // if (isEngaged && !isMotivatorFunctional()) {
+            // System.out.println("CRITICAL: Motivator failure during hyperspace!");
+            // emergencyStop();
         }
         
       
-        }
-    }
-}
+} // End of Class
