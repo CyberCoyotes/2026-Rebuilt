3f24a4a069e1acf1e3bec6e380ae26b89dd7d205
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2026-01-24 10:06:38 -0500

    Refactor shooter subsystem: reorganize imports, remove deprecated class, and implement new state machine logic

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.vision.VisionSubsystem;
 import frc.robot.subsystems.indexer.IndexerSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 /**
  * ShooterCommands - Factory for shooter-related commands.
