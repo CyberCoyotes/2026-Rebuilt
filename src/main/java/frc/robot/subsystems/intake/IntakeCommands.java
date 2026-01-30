3bcd5554a44aac2f72afb3be06ba2b67206349af
MMaaser <mmaaser1@reedcityschools.org>
2026-01-30 13:29:59 -0500

    Added commands

--- src/main/java/frc/robot/subsystems/intake/IntakeCommands.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeCommands.java
@@ -8,11 +8,11 @@ public class IntakeCommands {
     
     private IntakeCommands(){}
 
-   /*  public Command enterIntakeMode(IntakeSubsystem intake){ //should be default command
+     public Command enterIntakeMode(IntakeSubsystem intake){ //should be default command
         return 
         new  FunctionalCommand ( //TODO rapidly extending the slide whenever an object is close to the robot is a horrible idea
        () -> intake.setSlidePosition(IntakeConstants.SLIDE_EXTENDED_POSITION), //runs on init, extends slider
-       () -> intake.setRotatorVelocity(IntakeConstants.ROTATOR_RUNNING_VELOCITY), // runs until command ends, runs rotator
+       () -> intake.setRotatorSpeed(IntakeConstants.ROTATOR_RUNNING_VELOCITY), // runs until command ends, runs rotator
        interrupted -> intake.toRestingState(), // when the command is interrupted, return the subsystem to resting state (unexteded slider and stopped rotator)
        () -> intake.intakeTargetClose() && intake.indexerTargetClose(), // stop the command when there is nothing to be picked up and nothing inside it
        intake // the subsystem that the command depends on
@@ -20,6 +20,6 @@ public class IntakeCommands {
         }
 
     public Command stopJam(IntakeSubsystem intake){
-        return Commands.startEnd(() -> intake.setRotatorVelocity(-IntakeConstants.ROTATOR_MAX_VELOCITY),() -> intake.setRotatorVelocity(0), intake).withTimeout(0.5);
-    }*/
+        return Commands.startEnd(() -> intake.setRotatorSpeed(-IntakeConstants.ROTATOR_MAX_VELOCITY),() -> intake.setRotatorSpeed(0), intake).withTimeout(0.5);
+    }
     }
--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -3,18 +3,9 @@ package frc.robot.subsystems.intake;
 import org.littletonrobotics.junction.Logger;
 
 import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.playingwithfusion.TimeOfFlight;
-
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.subsystems.intake.IntakeIO.IntakeIOInputs;
 
 public class IntakeSubsystem extends SubsystemBase{   
 
