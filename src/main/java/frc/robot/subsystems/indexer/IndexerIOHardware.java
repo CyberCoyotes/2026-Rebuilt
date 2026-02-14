a1e1ed45b6a4ee24357a72e9d4b64e38c592c251
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2026-02-13 22:49:24 -0500

    refactor: remove unused suppression warning in IndexerIOHardware

--- src/main/java/frc/robot/subsystems/indexer/IndexerIOHardware.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOHardware.java
@@ -28,7 +28,6 @@ import frc.robot.util.TalonFXConfigs;
  * @see IndexerSubsystemBasic for a simpler direct-hardware approach (good for learning)
  */
 
-@SuppressWarnings("unused") // Suppress warnings for unused right now
 
 public class IndexerIOHardware implements IndexerIO {
 
