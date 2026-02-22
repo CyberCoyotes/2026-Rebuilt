59ad9879609f14f0739463c10d990e3d061990b8
Evan M <evanmcdonald5505@gmail.com>
2026-02-22 16:38:54 -0500

    Update CommandSwerveDrivetrain.java

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -24,12 +24,11 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 
 import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
 
@@ -51,7 +50,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private final SwerveRequest.SysIdSwerveSteerGains m_steerCharacterization = new SwerveRequest.SysIdSwerveSteerGains();
     private final SwerveRequest.SysIdSwerveRotation m_rotationCharacterization = new SwerveRequest.SysIdSwerveRotation();
 
-    // Field3d for Elastic Dashboard
+    // ===== Field2d for Elastic dashboard =====
     private final Field2d m_field = new Field2d();
 
     private final SysIdRoutine m_sysIdRoutineTranslation = new SysIdRoutine(
@@ -108,6 +107,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         SwerveModuleConstants<?, ?, ?>... modules
     ) {
         super(drivetrainConstants, modules);
+        SmartDashboard.putData("Field", m_field);
         if (Utils.isSimulation()) startSimThread();
     }
 
@@ -117,6 +117,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         SwerveModuleConstants<?, ?, ?>... modules
     ) {
         super(drivetrainConstants, odometryUpdateFrequency, modules);
+        SmartDashboard.putData("Field", m_field);
         if (Utils.isSimulation()) startSimThread();
     }
 
@@ -128,6 +129,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         SwerveModuleConstants<?, ?, ?>... modules
     ) {
         super(drivetrainConstants, odometryUpdateFrequency, odometryStandardDeviation, visionStandardDeviation, modules);
+        SmartDashboard.putData("Field", m_field);
         if (Utils.isSimulation()) startSimThread();
     }
 
@@ -183,6 +185,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 
     @Override
     public void periodic() {
+        // Update the field widget with the robot's current estimated pose
+        m_field.setRobotPose(getState().Pose);
+
         if (!m_hasAppliedOperatorPerspective || DriverStation.isDisabled()) {
             DriverStation.getAlliance().ifPresent(allianceColor -> {
                 setOperatorPerspectiveForward(
