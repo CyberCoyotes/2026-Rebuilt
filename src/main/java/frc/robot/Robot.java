293ca6a2a2a65031372670e64eb490b96329ceee
JediScoy <coyotepod@gmail.com>
2026-02-24 16:52:13 -0500

    Adjusting Contstants, nothing to see here.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.RobotController;
 // import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.vision.LimelightHelpers;
 
 public class Robot extends LoggedRobot {
     private Command m_autonomousCommand;
